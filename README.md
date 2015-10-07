42562b51988904ccc96694655b2b2819ecfb0bf9
Joscha Feth <jfeth@atlassian.com>
2015-10-07 17:48:07 +1100

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+use `node --harmony index.js`
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+let bucket = 'aui-origin.herokuapp.com';
+
+const debug = require('debug')('s3-batch')
+const error = require('debug')('s3-batch:error')
+
+const AWS = require('aws-sdk');
+const objectAssign = require('object-assign');
+const _ = require('lodash')
+
+let s3 = new AWS.S3({
+  params: {
+    Bucket: bucket
+  }
+});
+
+function list(marker) {
+  let params = {
+    Marker: marker,
+    Prefix: 'test',
+    //Delimiter: '/'
+  };
+  s3.listObjects(params, (err, data) => {
+    if(err) {
+      error(err, err.stack);
+      process.exit(1);
+    }
+    data.Contents.forEach((object) => {
+      if(object.Key.substr(-1) === '/') {
+        // it is a "folder"
+        return;
+      }
+      debug('object', object);
+      s3.headObject({
+        Bucket: bucket,
+        Key: object.Key
+      }, (err, headData) => {
+        if(err) {
+          error(err, err.stack);
+        }
+        debug('headData', headData);
+        let cacheControl = 'public, max-age=201';
+
+        var newMeta = objectAssign({}, headData.Metadata, {
+          'cache-control': cacheControl
+        });
+        if(_.isEqual(headData.Metadata, newMeta) && cacheControl === object.CacheControl) {
+          debug(`Metadata and Cache-Control is same for '${object.Key}'`);
+          return;
+        }
+        debug('newMeta', newMeta);
+
+        s3.copyObject({
+          Bucket: bucket,
+          CopySource: encodeURIComponent(`${bucket}/${object.Key}`),
+          Key: object.Key,
+          MetadataDirective: 'REPLACE',
+          CacheControl: cacheControl,
+          Metadata: newMeta
+        }, (err, copyData) => {
+          if(err) {
+            error(err, err.stack);
+          }
+          debug('copyData', copyData);
+        })
+      })
+    });
+    if(data.IsTruncated) {
+      list(data.NextMarker);
+    }
+  });
+}
+
+list();
